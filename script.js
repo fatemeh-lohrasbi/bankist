454884fb9837d6b245e8da62a4aa3d6d3771a39a
fatemeh lohrasbi <lohrasbi.developer@gmail.com>
2023-02-11 14:48:56 +0330

    change usernames

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Fatemeh Lohrasbi',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -22,7 +22,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Roze Finch',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
